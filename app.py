c2d6d0c9d4dedf7dd44bef1b1bb84fa969f16bc7
igormat7096 <igornovais.tj96@gmail.com>
2024-10-04 20:36:16 -0300

    app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ while True:
             }
 
             usuarios.append(usuario)
-            print(f"Usuário {usuario.get('Nome')} cadastrado com sucesso!")
+            print(f"Usuário(a) {usuario.get('Nome')} cadastrado(a) com sucesso!")
 
         case '2':
             print("\nUsuários cadastrados:")
